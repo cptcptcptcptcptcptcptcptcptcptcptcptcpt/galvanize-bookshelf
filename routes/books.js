65612c982991f89e172d8dfd8d2271e1dc7fd4a7
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:12:22 -0600

    AutomaticGitCommit

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const knex = require('../knex');
 const h = require('humps');
 r.route('/')
 .get((req, res) => {
-  knex('books').orderBy('title', 'asc').then((demBooks) => { res.send(h.camelizeKeys(demBooks)); })
+  knex('books').orderBy('title', 'asc').then((demBooks)=>{ res.send(h.camelizeKeys(demBooks)); })
 }).post((req, res) => {
   knex('books').returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
     .insert(h.decamelizeKeys(req.body)).then((book) => { res.send(h.camelizeKeys(book[0])); })
@@ -19,5 +19,4 @@ r.route('/:id')
 }).delete((req, res) => {
   knex('books').where('id', req.params.id).returning(['title', 'author', 'genre', 'description', 'cover_url'])
     .del().then((book) => { res.send(h.camelizeKeys(book[0])); });
-});
-module.exports = r;
+}); module.exports = r;
