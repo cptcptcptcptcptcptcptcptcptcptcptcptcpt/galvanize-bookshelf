72c5b4a746efbb394d95490b407205d899591d31
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 01:37:53 -0600

    AutomaticGitCommit

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,6 @@ r.route('/').get((req,res)=>{k('books').orderBy('title','asc').then((demBooks)=>
     .insert(h.decamelizeKeys(req.body)).then((book)=>{res.send(h.camelizeKeys(book[0]));});});
 r.route('/:id').get((req,res)=>{k('books').where('id',req.params.id).then((book)=>{res.send(h.camelizeKeys(book[0])); })})
 .patch((req,res)=>{k('books').where('id',req.params.id).returning(['id','title','author','genre','description','cover_url'])
-    .update(h.decamelizeKeys(req.body)).then((book) => { res.send(h.camelizeKeys(book[0])); });})
+    .update(h.decamelizeKeys(req.body)).then((book) => { res.send(h.camelizeKeys(book[0]));});})
 .delete((req,res)=>{k('books').where('id',req.params.id).returning(['title','author','genre','description','cover_url'])
     .del().then((book) => { res.send(h.camelizeKeys(book[0])); });}); module.exports = r;
