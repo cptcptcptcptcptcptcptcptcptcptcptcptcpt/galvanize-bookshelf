532ccbe769a942c8fb94f4930f4c271792063574
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 01:01:31 -0600

    AutomaticGitCommit

--- routes/favorites.js
+++ routes/favorites.js
@@ -8,9 +8,9 @@ r.route('/').get((req, res, next) => {knex('favorites').join('books', 'books.id'
    !req.cookies.token?next(bam):res.send(h(g)); })
   }).post((req,res,next)=>{knex('favorites').returning(['id','book_id','user_id']).insert({book_id:req.body.bookId,user_id:1})
     .then((p)=>{!req.cookies.token?next(bam):res.send(h(p[0])); })
-  }).delete((req,res,next)=>{!req.cookies.token?next(bam):knex('favorites').where('book_id',req.body.bookId).returning(['book_id','user_id']).del()
-      .then((rm) => {res.send(h(rm[0]));})
+  }).delete((req,res,next)=>{!req.cookies.token?next(bam):knex('favorites').where('book_id',req.body.bookId)
+  .returning(['book_id','user_id']).del().then((rm) => {res.send(h(rm[0]));})
   })
-r.route('/check').get((req, res, next) => {
-  !req.cookies.token?(req.query.bookId == 1?next(bam):next(bam)):(req.query.bookId == 1?res.send(true):res.send(false));
+r.route('/check').get((req, res, next) => {!req.cookies.token?(req.query.bookId == 1?
+  next(bam):next(bam)):(req.query.bookId == 1?res.send(true):res.send(false));
 });  module.exports = r;
