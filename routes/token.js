5d185a1cc876bd1e84f5580963eab1dd554f4e42
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:48:53 -0600

    AutomaticGitCommit

--- routes/token.js
+++ routes/token.js
@@ -7,5 +7,4 @@ r.route('/').get((req, res) => {!req.cookies.token ? res.send(false) : res.send(
   let usrO = {id:usr[0].id,firstName:usr[0].first_name,lastName:usr[0].last_name,email:usr[0].email};
   let token = jwt({email:usrO.email},'secret'); usrO.email===req.body.email&&b(req.body.password, usr[0].hashed_password)?
   res.cookie('token', token, {httpOnly:true}).send(usrO):next(bam);
-  })
-}).delete((req, res, next) => {res.clearCookie('token').send(true);}); module.exports = r;
+});}).delete((req, res, next) => {res.clearCookie('token').send(true);}); module.exports = r;
