7101ab4be2186a748ff9611c7bacac35b0b8f352
ctaggart27 <ctaggart27@gmail.com>
2017-04-20 23:14:23 -0600

    AutomaticGitCommit

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,7 +4,8 @@ const humps = require('humps').camelizeKeys;
 const bam = require('boom').create(401, 'Unauthorized');
 
 router.route('/')
-  .get((req, res, next) => { knex('favorites').join('books', 'books.id', 'book_id').then((g) => { !req.cookies.token ? next(bam) : res.send(humps(g)); })
+  .get((req, res, next) => { knex('favorites').join('books', 'books.id', 'book_id').then((g) => {
+    !req.cookies.token ? next(bam) : res.send(humps(g)); })
   }).post((req, res, next) => { knex('favorites').returning(['id', 'book_id', 'user_id']).insert({ book_id: req.body.bookId, user_id: 1 }).then((p) => {
       !req.cookies.token ? next(bam) : res.send(humps(p[0])); })
   }).delete((req, res, next) => { !req.cookies.token ? next(bam) :
