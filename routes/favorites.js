f60d40a49ddd2c4ae225ac83f9d55f4911be7095
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 01:05:15 -0600

    AutomaticGitCommit

--- routes/favorites.js
+++ routes/favorites.js
@@ -7,6 +7,6 @@ r.route('/').get((req, res, next) => {k('favorites').join('books', 'books.id', '
   }).delete((req,res,next)=>{!req.cookies.token?next(bam):k('favorites').where('book_id',req.body.bookId)
   .returning(['book_id','user_id']).del().then((rm) => {res.send(h(rm[0]));})
   })
-r.route('/check').get((req, res, next) => {!req.cookies.token?(req.query.bookId == 1?next(bam):next(bam)):
-  (req.query.bookId == 1?res.send(true):res.send(false));
+r.route('/check').get((req, res, next)=>{!req.cookies.token?(req.query.bookId==1?next(bam):next(bam)):
+  (req.query.bookId==1?res.send(true):res.send(false));
 });  module.exports = r;
