5b490e73e853e0ff3485e82d4fe8532ef5701ed8
ctaggart27 <ctaggart27@gmail.com>
2017-04-20 22:58:16 -0600

    AutomaticGitCommit

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex')
-const humps = require('humps').camelizeKeys();
+const humps = require('humps').camelizeKeys;
 const bam = require('boom').create(401, 'Unauthorized');
 
 router.route('/')
   .get((req, res, next) => {
     knex('favorites').join('books', 'books.id', 'book_id').then((g) => {
-      !req.cookies.token ? next(bam) : res.send(humps.(g))
+      !req.cookies.token ? next(bam) : res.send(humps(g))
     })
   })
   .post((req, res, next) => {
@@ -16,13 +16,13 @@ router.route('/')
       book_id: req.body.bookId,
       user_id: 1
     }).then((p) => {
-      !req.cookies.token ? next(bam) : res.send(humps.camelizeKeys(p[0]))
+      !req.cookies.token ? next(bam) : res.send(humps(p[0]))
     })
   })
   .delete((req, res, next) => {
     !req.cookies.token ? next(bam) :
       knex('favorites').where('book_id', req.body.bookId).returning(['book_id', 'user_id']).del().then((rm) => {
-        res.send(humps.camelizeKeys(rm[0]))
+        res.send(humps(rm[0]))
       })
   })
 router.route('/check')
