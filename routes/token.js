a5af4336ffd972194a946e91a520084e8dd2e998
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:48:28 -0600

    AutomaticGitCommit

--- routes/token.js
+++ routes/token.js
@@ -2,8 +2,7 @@
 const b = require('bcrypt').compareSync;
 const jwt = require('jsonwebtoken').sign;
 const bam = require('boom').create(400, 'Bad email or password');
-r.route('/')
-.get((req, res) => {!req.cookies.token ? res.send(false) : res.send(true);})
+r.route('/').get((req, res) => {!req.cookies.token ? res.send(false) : res.send(true);})
 .post((req, res, next) => {knex('users').select().then((usr) => {
   let usrO = {id:usr[0].id,firstName:usr[0].first_name,lastName:usr[0].last_name,email:usr[0].email};
   let token = jwt({email:usrO.email},'secret'); usrO.email===req.body.email&&b(req.body.password, usr[0].hashed_password)?
