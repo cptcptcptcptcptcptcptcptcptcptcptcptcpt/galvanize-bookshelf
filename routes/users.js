769b9152542fb4f19b757ae63e9a03b63caee236
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:20:03 -0600

    AutomaticGitCommit

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const h = require('humps').camelizeKeys;
 const b = require('bcrypt').hashSync;
 r.route('/').post((req, res) => {
   knex('users').returning(['id', 'first_name', 'last_name', 'email'])
-    .insert({first_name: req.body.firstName, last_name: req.body.lastName, email: req.body.email, hashed_password: b(req.body.password, 1), created_at: new Date(), updated_at: new Date()
+    .insert({first_name: req.body.firstName, last_name: req.body.lastName, email: req.body.email,
+       hashed_password: b(req.body.password, 1), created_at: new Date(), updated_at: new Date()
     }).then((usr) => { res.send(h(usr[0])); });
 }); module.exports = r;
