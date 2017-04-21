ac1421356ac4bc7142c023c092da093ce50f78e4
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:00:52 -0600

    AutomaticGitCommit

--- routes/token.js
+++ routes/token.js
@@ -1,15 +1,13 @@
 'use strict';
 const express = require('express');
-const router = express.Router();
+const r = express.Router();
 const knex = require('../knex');
 const bcrypt = require('bcrypt').compareSync;
 const jwt = require('jsonwebtoken').sign;
 const bam = require('boom').create(400, 'Bad email or password');
-router.route('/').get((req, res, next) => {
-  !req.cookies.token ? res.status(200).send(false) : res.status(200).send(true);
+r.route('/').get((req, res, next) => {
+  !req.cookies.token ? res.send(false) : res.send(true);
 }).post((req, res, next) => {
-  let email = req.body.email;
-  let pass = req.body.password;
   knex('users').select().then((usr) => {
     let usrO = {
       id: usr[0].id,
@@ -17,17 +15,12 @@ router.route('/').get((req, res, next) => {
       lastName: usr[0].last_name,
       email: usr[0].email
     };
-    let p = usr[0].hashed_password;
-    let e = usrO.email;
-    let token = jwt({
-      email: e
-    }, 'secret');
-    e === email && bcrypt(pass, p) ? res.cookie('token', token, {
-      httpOnly: true
-    }).send(usrO) : next(bam);
+    let token = jwt({ email: usrO.email }, 'secret');
+    usrO.email === req.body.email && bcrypt(req.body.password, usr[0].hashed_password)?res.cookie('token', token, {
+      httpOnly: true }).send(usrO) : next(bam);
   })
 }).delete((req, res, next) => {
   res.clearCookie('token');
-  res.status(200).send(true)
+  res.send(true)
 });
-module.exports = router;
+module.exports = r;
