1b39376d7c5fc6e7cec12a76f5c5874ab319682f
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 09:23:16 -0600

    AutomaticGitCommit

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,34 @@
-'use strict';const e=require('express');const r=e.Router();const k=require('../knex');const h=require('humps');
-r.route('/').get((req,res)=>{k('books').orderBy('title','asc').then((demBooks)=>{res.send(h.camelizeKeys(demBooks));});})
-.post((req,res)=>{k('books').returning(['id','title','author','genre','description','cover_url'])
-    .insert(h.decamelizeKeys(req.body)).then((book)=>{res.send(h.camelizeKeys(book[0]));});});
-r.route('/:id').get((req,res)=>{k('books').where('id',req.params.id).then((book)=>{res.send(h.camelizeKeys(book[0]));})})
-.patch((req,res)=>{k('books').where('id',req.params.id).returning(['id','title','author','genre','description','cover_url'])
-    .update(h.decamelizeKeys(req.body)).then((book)=>{res.send(h.camelizeKeys(book[0]));});})
-.delete((req,res)=>{k('books').where('id',req.params.id).returning(['title','author','genre','description','cover_url'])
-    .del().then((book)=>{res.send(h.camelizeKeys(book[0]));});}); module.exports=r;
+'use strict';
+const e = require('express');
+const r = e.Router();
+const k = require('../knex');
+const h = require('humps');
+r.route('/').get((req, res) => {
+    k('books').orderBy('title', 'asc').then((demBooks) => {
+      res.send(h.camelizeKeys(demBooks));
+    });
+  })
+  .post((req, res) => {
+    k('books').returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+      .insert(h.decamelizeKeys(req.body)).then((book) => {
+        res.send(h.camelizeKeys(book[0]));
+      });
+  });
+r.route('/:id').get((req, res) => {
+    k('books').where('id', req.params.id).then((book) => {
+      res.send(h.camelizeKeys(book[0]));
+    })
+  })
+  .patch((req, res) => {
+    k('books').where('id', req.params.id).returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+      .update(h.decamelizeKeys(req.body)).then((book) => {
+        res.send(h.camelizeKeys(book[0]));
+      });
+  })
+  .delete((req, res) => {
+    k('books').where('id', req.params.id).returning(['title', 'author', 'genre', 'description', 'cover_url'])
+      .del().then((book) => {
+        res.send(h.camelizeKeys(book[0]));
+      });
+  });
+module.exports = r;
