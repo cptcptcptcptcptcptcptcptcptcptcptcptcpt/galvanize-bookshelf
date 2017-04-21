e82527b0b08b218b80f9ea2673cba1631ab47796
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:45:14 -0600

    AutomaticGitCommit

--- routes/token.js
+++ routes/token.js
@@ -5,9 +5,8 @@ const bam = require('boom').create(400, 'Bad email or password');
 r.route('/')
 .get((req, res) => {!req.cookies.token ? res.send(false) : res.send(true);})
 .post((req, res, next) => {knex('users').select().then((usr) => {
-    let usrO = {id:usr[0].id,firstName:usr[0].first_name,lastName:usr[0].last_name,email:usr[0].email};
-    let token = jwt({email:usrO.email},'secret');
-    usrO.email===req.body.email&&b(req.body.password, usr[0].hashed_password)?
-    res.cookie('token', token, {httpOnly:true}).send(usrO):next(bam);
+  let usrO = {id:usr[0].id,firstName:usr[0].first_name,lastName:usr[0].last_name,email:usr[0].email};
+  let token = jwt({email:usrO.email},'secret'); usrO.email===req.body.email&&b(req.body.password, usr[0].hashed_password)?
+  res.cookie('token', token, {httpOnly:true}).send(usrO):next(bam);
   })
 }).delete((req, res, next) => {res.clearCookie('token').send(true);}); module.exports = r;
