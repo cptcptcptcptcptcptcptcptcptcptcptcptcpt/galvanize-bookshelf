dbebfc2c93db77dc683420ad2d1bb4dbf3c02bd5
ctaggart27 <ctaggart27@gmail.com>
2017-04-20 23:09:35 -0600

    AutomaticGitCommit

--- routes/favorites.js
+++ routes/favorites.js
@@ -17,14 +17,13 @@ router.route('/')
     }).then((p) => {
       !req.cookies.token ? next(bam) : res.send(humps(p[0]))
     })
-  })
-  .delete((req, res, next) => {
+  }).delete((req, res, next) => {
     !req.cookies.token ? next(bam) :
       knex('favorites').where('book_id', req.body.bookId).returning(['book_id', 'user_id']).del().then((rm) => {
         res.send(humps(rm[0]))
       })
   })
 router.route('/check').get((req, res, next) => {
-  !req.cookies.token ? (req.query.bookId == 1 ? next(bam) : next(bam)) : (req.query.bookId == 1 ? res.send(true) : res.send(false))
+  !req.cookies.token ? (req.query.bookId == 1 ? next(bam) : next(bam)) : (req.query.bookId == 1 ? res.send(true) : res.send(false));
 })
 module.exports = router;
