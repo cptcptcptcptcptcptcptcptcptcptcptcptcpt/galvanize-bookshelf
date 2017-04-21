56b2192a27293a7ca7364c698792a22d863b6919
ctaggart27 <ctaggart27@gmail.com>
2017-04-20 23:29:39 -0600

    AutomaticGitCommit

--- routes/books.js
+++ routes/books.js
@@ -1,34 +1,33 @@
 'use strict';
 const express = require('express');
-const router = express.Router();
+const r = express.Router();
 const knex = require('../knex');
-const humps = require('humps');
-//HANDLING ALL MY ROUTING @ ONCE 👨🏻 LiL_Code
-router.route('/')
+const h = require('humps');
+r.route('/')
 .get((req, res) => {
   knex('books').orderBy('title', 'asc').then((demBooks) => {
-    res.send(humps.camelizeKeys(demBooks))
+    res.send(h.camelizeKeys(demBooks))
   })
 }).post((req, res) => {
   knex('books').returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
-    .insert(humps.decamelizeKeys(req.body)).then((book) => {
-      res.send(humps.camelizeKeys(book[0]));
+    .insert(h.decamelizeKeys(req.body)).then((book) => {
+      res.send(h.camelizeKeys(book[0]));
     })
 });
-router.route('/:id')
+r.route('/:id')
 .get((req, res) => {
     knex('books').where('id', req.params.id).then((book) => {
-    res.send(humps.camelizeKeys(book[0]));
+    res.send(h.camelizeKeys(book[0]));
   })
 }).patch((req, res) => {
   knex('books').where('id', req.params.id).returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
-    .update(humps.decamelizeKeys(req.body)).then((book) => {
-      res.send(humps.camelizeKeys(book[0]));
+    .update(h.decamelizeKeys(req.body)).then((book) => {
+      res.send(h.camelizeKeys(book[0]));
     });
 }).delete((req, res) => {
   knex('books').where('id', req.params.id).returning(['title', 'author', 'genre', 'description', 'cover_url'])
     .del().then((book) => {
-      res.send(humps.camelizeKeys(book[0]));
+      res.send(h.camelizeKeys(book[0]));
     });
 });
-module.exports = router;
+module.exports = r;
