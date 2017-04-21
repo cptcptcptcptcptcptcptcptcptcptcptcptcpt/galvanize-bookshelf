2a8b5bf226060d56720215761805643277eee1ef
ctaggart27 <ctaggart27@gmail.com>
2017-04-21 00:32:06 -0600

    AutomaticGitCommit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 'use strict'; const express = require('express'); const r = express.Router(); const knex = require('../knex');
-const h = require('humps').camelizeKeys;
-const b = require('bcrypt').hashSync;
+const h = require('humps').camelizeKeys; const b = require('bcrypt').hashSync;
 r.route('/').post((req,res)=>{knex('users').returning(['id','first_name','last_name','email']).insert({
   first_name:req.body.firstName,last_name:req.body.lastName,email:req.body.email,hashed_password:b(req.body.password, 1),
   created_at:new Date(),updated_at:new Date()}).then((usr)=>{res.send(h(usr[0]));});
